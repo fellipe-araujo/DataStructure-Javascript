5a5d9bc6b07c96b04ef64b143c250270a556324d
Fellipe <araujo.fellipe@hotmail.com>
2021-02-11 00:52:47 -0300

    Add example

--- graph.js
+++ graph.js
@@ -93,6 +93,31 @@ function Graph() {
   };
 }
 
-function printNode(value) {
+function visitVertex(value) {
   console.log('Visited vertex: ' + value);
 }
+
+// EXEMPLO PRÁTICO
+var graph = new Graph();
+var vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+for (var i = 0; i < vertices.length; i++) {
+  graph.addVertex(vertices[i]);
+}
+
+graph.addEdge('A', 'B')
+graph.addEdge('A', 'C')
+graph.addEdge('A', 'D')
+graph.addEdge('C', 'D')
+graph.addEdge('C', 'G')
+graph.addEdge('D', 'G')
+graph.addEdge('D', 'H')
+graph.addEdge('B', 'E')
+graph.addEdge('B', 'F')
+graph.addEdge('E', 'I')
+
+console.log('BUSCA EM PROFUNDIDADE');
+graph.dfs(visitVertex)
+
+console.log('\nBUSCA EM LARGURA');
+graph.bfs(vertices[0], visitVertex)
